a9bdd04766930f8fc677fa52de358296626114d5
eeetanju <tanjuvonal@gmail.com>
2017-08-09 21:21:07 +0300

    yeni rest example

---
+++ RestWSExample/RestWSExample/Controllers/PersonController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RestWSExample.Models;
+
+namespace RestWSExample.Controllers
+{
+    public class PersonController : ApiController
+    {
+        [HttpGet]
+        public string getName(string ad,string soyad)
+        {
+            return "Ad: " + ad + " Soyad: " + soyad;
+        }
+
+        [HttpGet]
+        public List<Person> getPersons(string ad)
+        {
+            List<Person> prsns = new List<Person>();
+            prsns.Add(new Person { name = "tanju", surname = "vonal" });
+            prsns.Add(new Person { name = "ali", surname = "veli" });
+            return prsns;
+        }
+    }
+}
---
+++ RestWSExample/RestWSExample/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestWSExample.Models
+{
+    public class Person
+    {
+        public String name { get; set; }
+        public String surname { get; set; }
+        public String email { get; set; }
+    }
+}
